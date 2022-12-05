cd1b1b3eb0db6f2c8757c94d49c3cda801ac7b6e
gope-27 <117707851+gope-27@users.noreply.github.com>
2022-12-05 16:11:13 +0530

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 from pandas import *
-
+import plotly.graph_objects as go
 import altair as alt
 
 
@@ -29,6 +29,7 @@ Dep = st.sidebar.multiselect(
     default=df["Department"].unique(),
 )
 
+
 SubDep = st.sidebar.multiselect(
     "Select the SubDepartment:",
     options=df["SubDepartment"].unique(),
@@ -47,6 +48,7 @@ df_selection = df.query(
     "Department == @Dep & SubDepartment ==@SubDep & Month == @Mon"
 )
 
+
 # ---- MAINPAGE ----
 st.title(":bar_chart:Supply Chain Dashboard")
 st.markdown("##")
@@ -58,9 +60,8 @@ Orders_for_next_3days = df_selection[(df_selection['Next Order Flag'] >= 0) & (d
 Cost_for_next_3days = df_selection[(df_selection['Next Order Flag'] >= 0) & (df_selection['Next Order Flag'] <= 3)].sum()["Unit cost"]
 left_column, middle_column, right_column = st.columns(3)
 
+
 with left_column:   
-    # st.markdown("Previous year Overall AVG Unit Storage Cost")
-    # st.markdown(f" $ {Previous_year_Overall_Average_Unit_Storage_Cost}")
     st.markdown("<h4 style='text-align: center; color: black;'> Overall AVG Unit Storage Cost</h4>", unsafe_allow_html=True)
     st.markdown("<h4 style='text-align: center; color: blue;'>"+"$ "+str((float("{:.2f}".format(Overall_Average_Unit_Storage_Cost ))))+"</h4>", unsafe_allow_html=True)  
 
@@ -74,45 +75,49 @@ with right_column:
     st.markdown("<h4 style='text-align: center; color: black;'>Cost For Next 3days </h4>", unsafe_allow_html=True)
     st.markdown("<h4 style='text-align: center; color: blue;'>"+"$ "+str(round(Cost_for_next_3days  /1000,2))+"K"+"</h4>", unsafe_allow_html=True)  
 
-
-
 st.markdown("""---""")
 
 # #CHART_1
-Storage_Cost_By_Month= df_selection.groupby(by=["Month"]).mean()[["Unit Storage Cost"]].reset_index()
-fig = px.line(Storage_Cost_By_Month,x = 'Month',y='Unit Storage Cost',
-title="<b>Comparison Of AVG Unit Storage Cost By Sub-Department</b>"
+Storage_Cost_By_Month= df.groupby(by=["Month"]).mean()[["Unit Storage Cost"]].round(2).reset_index()
+fig = px.line(Storage_Cost_By_Month,x = 'Month',y='Unit Storage Cost',text=(Storage_Cost_By_Month['Unit Storage Cost']),
+title="<b>Comparison Of Unit Storage Cost By Month</b>"
 )
+fig.update_layout(xaxis_title="Month",
+yaxis_title="AVG Unit Storage Cost",
+plot_bgcolor="rgba(0,0,0,0)",
+yaxis=(dict(showgrid=False)),
+xaxis=(dict(showgrid=False))
+)
+fig.update_traces(marker_color="#3EC1CD")
+
 
 st.plotly_chart(fig,use_container_width=True)
 
 #CHART-2
-Storage_Cost_By_Month= df_selection.groupby(by=["SubDepartment"]).mean()[["Unit Storage Cost"]]
-SubDepartment_unit_Storge_Cost = px.bar(Storage_Cost_By_Month,
-    x= Storage_Cost_By_Month.index,
-    y="Unit Storage Cost",
-    title="<b> Comparison Of AVG Unit Storage Cost By Month</b>",
-    template="plotly_white"
-)
-SubDepartment_unit_Storge_Cost.update_layout(
-xaxis=dict(tickmode="linear"),
+Storage_Cost_By_Month = df_selection.groupby(by=["SubDepartment"]).mean()[["Unit Storage Cost"]].round(2).reset_index()
+Storage_Cost_By_Month = Storage_Cost_By_Month.sort_values(by='Unit Storage Cost')
+
+fig1 = px.bar(Storage_Cost_By_Month, x='SubDepartment', y='Unit Storage Cost',orientation='v',title="<b>Comparison Of Unit Storage Cost By Sub-Department</b>"
+,text=(Storage_Cost_By_Month['Unit Storage Cost']))
+fig1.update_layout(xaxis_title="SubDepartment",
+yaxis_title="AVG Unit Storage Cost",
 plot_bgcolor="rgba(0,0,0,0)",
-yaxis=(dict(showgrid=False)),
-)
+yaxis=(dict(showgrid=False)))
+#fig1.update_traces(marker_color="#3EC1CD")
 
-st.plotly_chart(SubDepartment_unit_Storge_Cost,use_container_width=True)
 
+st.plotly_chart(fig1,use_container_width=True)
 
 #CHART-3
-Storage_Cost_By_Month = (
-    df_selection.groupby(by=["Department"]).mean()[["Fulfillment TAT post Order Placement(in days)"]].sort_values(by="Fulfillment TAT post Order Placement(in days)")
-)
-fig_hourly_sales = px.bar(
-    Storage_Cost_By_Month,
+Storage_Cost_By_Month = df_selection.groupby(by=["Department"]).mean()[["Fulfillment TAT post Order Placement(in days)"]].round(2)
+Storage_Cost_By_Month = Storage_Cost_By_Month .sort_values(by="Fulfillment TAT post Order Placement(in days)")
+
+fig_hourly_sales = px.bar(Storage_Cost_By_Month,
     x="Fulfillment TAT post Order Placement(in days)",
     y=Storage_Cost_By_Month.index,
     orientation="h",
     title="<b>Department Wise AVG Fulfillment TAT post Order Placement</b>",
+    text=(Storage_Cost_By_Month['Fulfillment TAT post Order Placement(in days)']),
     color_discrete_sequence=["#0083B8"] * len(Storage_Cost_By_Month),
     template="plotly_white",
 )
@@ -121,9 +126,22 @@ fig_hourly_sales.update_layout(
     xaxis=(dict(showgrid=False))
 )
 
+# Storage_Cost_By_Month = df_selection.groupby(by=["Department"]).mean()[["Fulfillment TAT post Order Placement(in days)"]].reset_index()#.sort_values(by="Fulfillment TAT post Order Placement(in days)")
+
+# labels = Storage_Cost_By_Month['Department']
+# values = Storage_Cost_By_Month['Fulfillment TAT post Order Placement(in days)']
+
+# colours = ["#0C3B5D", "#3EC1CD"]
+
+# fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.5)])
+# fig.update_traces(marker=dict(colors=colours),
+# legendgrouptitle_text="Gender")
+# fig.update_layout(height=500, width=600)
+
+
 #CHART-4
 Storage_Cost_By_Month = (
-    df_selection.groupby(by=["Department"]).mean()[["Unit Storage Cost","Target Unit Storage cost"]]#.sort_values(by="Fulfillment TAT post Order Placement(in days)")
+    df_selection.groupby(by=["Department"]).mean()[["Unit Storage Cost","Target Unit Storage cost"]].round(2)
 )
 fig_product_sales = px.bar(
     Storage_Cost_By_Month,
@@ -131,6 +149,7 @@ fig_product_sales = px.bar(
     y=Storage_Cost_By_Month.index,
     orientation="h",
     title="<b>Comparison Of Unit Storage Cost By Department</b>",
+    text=(Storage_Cost_By_Month["Unit Storage Cost"]),
     color_discrete_sequence=["#0083B8"] * len(Storage_Cost_By_Month),
     template="plotly_white",
 )
@@ -144,12 +163,12 @@ left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
 right_column.plotly_chart(fig_product_sales, use_container_width=True)
 
 #CHART-5
-Storage_Cost_By_Month= df_selection.groupby(by=["SubDepartment"]).mean()[["Fulfillment TAT post Order Placement(in days)"]]
+Storage_Cost_By_Month= df_selection.groupby(by=["SubDepartment"]).mean()[["Fulfillment TAT post Order Placement(in days)"]].sort_values("Fulfillment TAT post Order Placement(in days)").round(2)
 SubDepartment_unit_Storge_Cost = px.bar(Storage_Cost_By_Month,
     x= Storage_Cost_By_Month.index,
     y="Fulfillment TAT post Order Placement(in days)",
     title="<b> Sub-Department WiseFulfillment TAT post Order Placement</b>",
-    template="plotly_white"
+    text = 'Fulfillment TAT post Order Placement(in days)',
 )
 SubDepartment_unit_Storge_Cost.update_layout(
 xaxis=dict(tickmode="linear"),
@@ -226,54 +245,3 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
